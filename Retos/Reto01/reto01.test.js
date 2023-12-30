99be156d1c62a24a43763e1bd146e4264e401c38
Desiree <desireesanchezdev@gmail.com>
2023-12-30 01:29:57 +0100

    add test01 describe

--- Retos/Reto01/reto01.test.js
+++ Retos/Reto01/reto01.test.js
@@ -1,21 +1,22 @@
-
 const findFirstRepeated = require("./reto01.js")
 
-test('findFirstRepeated retorna el primer numero repetido del ejemplo 1', () => {
-    const giftIds = [2, 1, 3, 5, 3, 2];
-    const result = findFirstRepeated(giftIds);
-    expect(result).toBe(3);
-});
+describe("findFirstRepeated Function", () => {
 
-test('findFirstRepeated retorna -1 porque no hay ningun número repetido', () => {
-    const giftIds2 = [1, 2, 3, 4];
-    const result = findFirstRepeated(giftIds2);
-    expect(result).toBe(-1);
-});
+    test('findFirstRepeated retorna el primer numero repetido del ejemplo 1', () => {
+        const giftIds = [2, 1, 3, 5, 3, 2];
+        const result = findFirstRepeated(giftIds);
+        expect(result).toBe(3);
+    });
 
-test('findFirstRepeated retorna el primer numero repetido del ejemplo 3', () => {
-    const giftIds3 = [5, 1, 5, 1];
-    const result = findFirstRepeated(giftIds3);
-    expect(result).toBe(5);
-});
+    test('findFirstRepeated retorna -1 porque no hay ningun número repetido', () => {
+        const giftIds2 = [1, 2, 3, 4];
+        const result = findFirstRepeated(giftIds2);
+        expect(result).toBe(-1);
+    });
 
+    test('findFirstRepeated retorna el primer numero repetido del ejemplo 3', () => {
+        const giftIds3 = [5, 1, 5, 1];
+        const result = findFirstRepeated(giftIds3);
+        expect(result).toBe(5);
+    });
+});
